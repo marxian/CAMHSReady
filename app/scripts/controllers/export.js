f12bb9d01f9749c509515a9d3dba88a0cb90a80d
Rupert Redington <rupert@neontribe.co.uk>
2013-05-24 16:27:33 +0100

    inital work for download

--- app/scripts/controllers/export.js
+++ app/scripts/controllers/export.js
@@ -2,8 +2,13 @@
 
 angular.module('docready')
   .controller('ExportCtrl', function ($scope, settings, symptomService, $window, $resource, $timeout) {
-    var Email = $resource(settings.apiRoot + '/email');
+    var Email = $resource(settings.apiRoot + '/email'),
+      Pdf = $resource(settings.apiRoot + '/pdf');
     $scope.symptoms = symptomService.symptoms;
+
+    function symptomList(){
+      return _.chain($scope.symptoms).where({selected: true}).pluck('title').value();
+    }
 
     $scope.prepareMail = function(){
       // Toggle our email object
@@ -12,16 +17,26 @@ angular.module('docready')
       } else {
         $scope.email = new Email({
           recipient: '',
-          symptoms: _.chain($scope.symptoms).where({selected: true}).pluck('title').value()
+          symptoms: symptomList()
         });
       }
     };
+
     $scope.sendEmail = function(){
       $scope.email.state = 'sending';
       $scope.email.$save(function(){
         $scope.email.state = 'sent';
         // close the send dialog after showing the sent state for a while
         $timeout(function(){ $scope.email = null; }, 1000);
+      });
+    };
+
+    $scope.getPdf = function(){
+      var pdf = new Pdf({
+        symptoms: symptomList()
+      });
+      pdf.$save(function(res){
+        $window.open(res.pdf);
       });
     };
 
